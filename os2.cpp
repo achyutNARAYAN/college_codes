8c798f425d49dd050bceb933101d81d1c43fe0d3
Achyut_narayan <121540524+achyutNARAYAN@users.noreply.github.com>
2024-04-02 15:18:52 +0530

    Add files via upload

---
+++ os2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+struct Process { 
+int pid, bt, priority; 
+}; 
+bool compare(Process a, Process b) { 
+return (a.priority > b.priority); 
+} 
+void findWaitingTime(Process proc[], int n, int wt[]) { 
+    wt[0] = 0; 
+    for (int i = 1; i < n; i++) 
+    wt[i] = proc[i - 1].bt + wt[i - 1]; 
+} 
+void findTurnAroundTime(Process proc[], int n, int wt[], int tat[]) { 
+    for (int i = 0; i < n; i++) 
+    tat[i] = proc[i].bt + wt[i]; 
+} 
+void findavgTime(Process proc[], int n) { 
+    int wt[n], tat[n], total_wt = 0, total_tat = 0; 
+    findWaitingTime(proc, n, wt); 
+    findTurnAroundTime(proc, n, wt, tat); 
+    for (int i = 0; i < n; i++) { 
+        total_wt += wt[i]; 
+        total_tat += tat[i]; 
+    } 
+    cout << "Order in which processes get executed: "; 
+    for (int i = 0; i < n; i++) 
+    cout << proc[i].pid << " "; 
+    cout << "\nProcesses  Burst time  Waiting time  Turn around time\n"; 
+    for (int i = 0; i < n; i++) 
+    cout << proc[i].pid << "\t\t" << proc[i].bt << "\t " << wt[i] << "\t\t " << tat[i] << endl; 
+    cout << "\nAverage waiting time = " << (float)total_wt / n; 
+    cout << "\nAverage turn around time = " << (float)total_tat / n; 
+} 
+void priorityScheduling(Process proc[], int n) { 
+    sort(proc, proc + n, compare); 
+    findavgTime(proc, n); 
+} 
+int main() { 
+    Process proc[] = { { 1, 10, 2 }, { 2, 5, 0 }, { 3, 8, 1 } }; 
+    int n = sizeof proc / sizeof proc[0]; 
+    priorityScheduling(proc, n); 
+    return 0; 
+}
